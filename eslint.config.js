68854aa6ab5138d45b5e4b28f802cc20bccb4e55
Andrii Oriekhov <andriyorehov@gmail.com>
2024-01-28 13:14:59 +0200

    enable Type Aware Rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,9 @@
 const antfu = require('@antfu/eslint-config').default
 
 module.exports = antfu({
+  typescript: {
+    tsconfigPath: 'tsconfig.json',
+  },
   overrides: {
     javascript: {
       'no-console': 'off',
