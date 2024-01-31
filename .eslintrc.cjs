9fd80eb34bebbc0d98c0fab4893e59abaacbb4d1
Andrii Oriekhov <andriyorehov@gmail.com>
2024-01-31 23:07:26 +0200

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,20 @@
 /* eslint-env node */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'etc'],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "etc"],
   root: true,
   parserOptions: {
     project: true,
     tsconfigRootDir: __dirname,
   },
   rules: {
+    "etc/no-const-enum": "error",
     "@typescript-eslint/switch-exhaustiveness-check": "error",
-    "etc/no-const-enum": "error"
-  }
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+  },
 };
